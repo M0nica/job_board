f7895c085b38ee2c29ac94ee3fc0572998655e17
Monica Powell <mmp2181@columbia.edu>
2018-03-24 14:07:57 -0400

    full ability to edit and delete jobs

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -15,6 +15,26 @@ class JobsController < ApplicationController
     # render :index
   end
 
+  # job is based off of the id param that is passed in the uRL
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+
+  def update
+    @job = Job.find(params[:id])
+    @job.update_attributes(job_params)
+    redirect_to jobs_path
+  end
+
+  def destroy
+    Job.find(params[:id]).destroy
+    #@job = Job.find(params[:id])
+    #@job.destroy
+    redirect_to jobs_path
+  end
+
+
   private # beginning of private methods -- anything below this line will be private
 
 # strong parameters to ignore malicious form entries
