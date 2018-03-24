408ace2141cde40af76af59e78952a2a96712da8
Monica Powell <mmp2181@columbia.edu>
2018-03-24 13:40:01 -0400

    functionality to add jobs and view them on the homepage

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,7 +1,27 @@
 class JobsController < ApplicationController
   def index
+    @jobs = Job.all
   end
   def new
+    # create an instance of the Job class in an @instance variable.
+    # instance variables have @ symbols and have a scope throughout the application
      @job = Job.new
   end
+  def create
+    Job.create(job_params)
+    # helper method to redirect to the /jobs_path
+    redirect_to jobs_path
+    # tell create to render the index html
+    # render :index
+  end
+
+  private # beginning of private methods -- anything below this line will be private
+
+# strong parameters to ignore malicious form entries
+# whitlisting/specifying the fields that we want!
+
+  def job_params
+    params.require(:job).permit(:title, :description)
+  end
+
 end
